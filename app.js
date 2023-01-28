ad76bdaeaff7d8c4a8b394ddfd534e298fcb4692
A13X-19 <arocha136@alamo.edu>
2023-01-27 20:37:49 -0800

    Added Variables

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
 console.log('Did I take a class with you recently: Yes or No?') ;
+
+var xan = 1000; // Note-Entry 01 - Global Scope ACTIVE
+
+xan = xan - 10; // I THINK I did it right??
+
+var name = 'A.J R 2023'; // Creator name displayed
+
+var checkout = true; // BOOLEAN Gemini
+
+var box = null; //OBJECT SYMBOL NULL
+
+
+{
+    const xan = 100
+}
+// Can't touch this block!
+
+
+console.log(xan);
+
+const xandjur = "I lost my sword, help.";
