3453d247342c940b4d2f643c9eaa6a519adef222
A13X-19 <arocha136@alamo.edu>
2023-03-10 18:31:28 -0800

    rock paper scissors

--- app.js
+++ app.js
@@ -1,44 +1,118 @@
-const names = ['Xan', 'Seer', 'Reyna', 'Shison', 'Toza']
-const age = '20';
-const job = 'mercenary';
-const extra = 'collecting';
-document.body.style.backgroundColor = "#fefbe9";
+const game = () => {
+    let pScore = 0;
+    let cScore = 0;
+    
+    //Start Game
+    const startGame = () =>{
+        const playBtn = document.querySelector(".intro button");
+        const introScreen = document.querySelector(".intro");
+        const match = document.querySelector(".match");
 
-console.log(`I didn't know the key by 1 was actually used, heh. Anyway my name is ${name}`);
-console.log(`My physical age is ${age} and I am currently a ${job} for my government. I specialize in ${extra} swords from people of interest.`);
+        playBtn.addEventListener("click", () => {
+            introScreen.classList.add("fadeOut");
+            match.classList.add("fadeIn");
+        });
+    };
+    //Match
+    const playMatch = () => {
+        const options = document.querySelector(".options buttion");
+        const playerHand = document.querySelector(".player-hand");
+        const computerHand = document.querySelector(".computer-hand");
+        const hands = document.querySelector('.hands img');
 
+        hands.forEach(hand => {
+            hand.addEventListener('animationend', function(){
+                this.style.animation = "";
+            });
+        });
+        //Comp options
+        const computerOptions = ["rock", "paper", "scissors"];
 
-for (name of names) {
-    console.log(name);
-    if(name === 'Shison') {
-        console.log('I know a person called Shison');
-        break;
+        options.forEach(option => {
+            option.addEventListener("click", function() {
+                //Comp Choice
+                const computerNumber = Math.floor(Math.random() * 3);
+                const computerChoice = computerOptions[computerNumber];
+                //Compare Hands
+               
+
+                setTimeout(() =>{
+                    compareHands(this.textContent, computerChoice);
+                    //Img update
+                    playerHand.src = `./assets/${this.textContent}.png`;
+                    computerHand.src = `./assets/${computerChoice}.png`;
+                }, 2000);
+
+                playerHand.style.animation = "shakePlayer 2s ease";
+                computerHand.style.animation = "shakeComputer 2s ease";
+            });
+        });
+    };
+
+    const updateScore = () => {
+        const playerScore = document.querySelector(".player-score p");
+        const computerScore = document.querySelector(".computer-score p");
+        playerScore.textContent = pScore;
+        computerScore.textContent = cScore;
     }
-}
 
-///
+    const compareHands = (playerChoice, computerChoice) => {
 
-const user = {
-    name: 'Xan',
-    age: 20,
-    relatioship: false,
-    purchases: ["phone", "pc", "coffee"],
-    name: function() {
-        return this.name + " " + this.age;
-    }
+        const winner = document.querySelector(".winner");
+        if (playerChoice === computerChoice) {
+            winner.textContent = "Wow you tied!";
+            return;
+        }
+
+        //Rocks
+        if (playerChoice === "rock") {
+            if (computerChoice === "scissors") {
+                winner.textContent = "Player wins!"
+                pScore++;
+                updateScore();
+                return;
+            } else {
+                winner.textContent = "Computer Wins!";
+                cScore++;
+                updateScore();
+                return;
+            }
+        }
+        //Paper
+        if (playerChoice === "paper") {
+            if (computerChoice === "scissors") {
+                winner.textContent = "Computer wins!"
+                cScore++;
+                updateScore();
+                return;
+            } else {
+                winner.textContent = "Player Wins!";
+                pScore++;
+                updateScore();
+                return;
+            }
+        }
+        //Scissors
+        if (playerChoice === "scissors") {
+            if (computerChoice === "rock") {
+                winner.textContent = "Computer wins!"
+                cScore++;
+                updateScore();
+                return;
+            } else {
+                winner.textContent = "Player Wins!";
+                pScore++;
+                updateScore();
+                return;
+            }
+        }
+    };
+
+    //Call all inner functions
+    startGame();
+    playMatch();
 };
 
-console.log(user.purchases);
-console.log(this);
 
-///
-
-const daily = ["Wake up", "Brush teeth", "Eat", "Write JS", "Play games", "Sleep"]
-
-console.log(daily);
-console.log(daily[4]);
-
-daily.push("I lied I don't sleep");
-
-
-document.write("Loop stopped! ");
+//Start Game Function
+game();
