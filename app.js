83a31c49a93be14db068e1a1acd8d9ec39d5963a
A13X-19 <arocha136@alamo.edu>
2023-04-06 23:40:41 -0700

    java easy project 2

--- app.js
+++ app.js
@@ -1,118 +1,34 @@
-const game = () => {
-    let pScore = 0;
-    let cScore = 0;
-    
-    //Start Game
-    const startGame = () =>{
-        const playBtn = document.querySelector(".intro button");
-        const introScreen = document.querySelector(".intro");
-        const match = document.querySelector(".match");
-
-        playBtn.addEventListener("click", () => {
-            introScreen.classList.add("fadeOut");
-            match.classList.add("fadeIn");
-        });
-    };
-    //Match
-    const playMatch = () => {
-        const options = document.querySelector(".options buttion");
-        const playerHand = document.querySelector(".player-hand");
-        const computerHand = document.querySelector(".computer-hand");
-        const hands = document.querySelector('.hands img');
-
-        hands.forEach(hand => {
-            hand.addEventListener("animationend", function() {
-                this.style.animation = "";
-            });
-        });
-        //Comp options
-        const computerOptions = ["rock", "paper", "scissors"];
-
-        options.forEach(option => {
-            option.addEventListener("click", function() {
-                //Comp Choice
-                const computerNumber = Math.floor(Math.random() * 3);
-                const computerChoice = computerOptions[computerNumber];
-                //Compare Hands
-               
-
-                setTimeout(() =>{
-                    compareHands(this.textContent, computerChoice);
-                    //Img update
-                    playerHand.src = `./assets/${this.textContent}.png`;
-                    computerHand.src = `./assets/${computerChoice}.png`;
-                }, 2000);
-
-                playerHand.style.animation = "shakePlayer 2s ease";
-                computerHand.style.animation = "shakeComputer 2s ease";
-            });
-        });
-    };
-
-    const updateScore = () => {
-        const playerScore = document.querySelector(".player-score p");
-        const computerScore = document.querySelector(".computer-score p");
-        playerScore.textContent = pScore;
-        computerScore.textContent = cScore;
+let data = [
+    {
+        name: 'Seer Imperium,',
+        age: '34'
+    },
+    {
+        name: 'Heir Reina,',
+        age: '31'
+    },
+    {
+        name: 'Xan Mischief,',
+        age: '20'
+    },
+    {
+        name: 'TOZA Shison,',
+        age: '19'
+    },
+    {
+        name: 'Capt. Archith,',
+        age: '27'
+    },
+    {
+        name: 'Lt. Kivut,',
+        age: '38'
     }
+];
 
-    const compareHands = (playerChoice, computerChoice) => {
+const info = document.querySelector('#info')
 
-        const winner = document.querySelector(".winner");
-        if (playerChoice === computerChoice) {
-            winner.textContent = "Wow you tied!";
-            return;
-        }
+let details = data.map(function(item) {
+    return '<div>' + item.name + ' ' + 'Age: ' + item.age + '</div>';
+});
 
-        //Rocks
-        if (playerChoice === "rock") {
-            if (computerChoice === "scissors") {
-                winner.textContent = "Player wins!"
-                pScore++;
-                updateScore();
-                return;
-            } else {
-                winner.textContent = "Computer Wins!";
-                cScore++;
-                updateScore();
-                return;
-            }
-        }
-        //Paper
-        if (playerChoice === "paper") {
-            if (computerChoice === "scissors") {
-                winner.textContent = "Computer wins!"
-                cScore++;
-                updateScore();
-                return;
-            } else {
-                winner.textContent = "Player Wins!";
-                pScore++;
-                updateScore();
-                return;
-            }
-        }
-        //Scissors
-        if (playerChoice === "scissors") {
-            if (computerChoice === "rock") {
-                winner.textContent = "Computer wins!"
-                cScore++;
-                updateScore();
-                return;
-            } else {
-                winner.textContent = "Player Wins!";
-                pScore++;
-                updateScore();
-                return;
-            }
-        }
-    };
-
-    //Call all inner functions
-    startGame();
-    playMatch();
-};
-
-
-//Start Game Function
-game();
+info.innerHTML = details.join('\n');
